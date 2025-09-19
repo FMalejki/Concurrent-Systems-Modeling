a893e20b8e26dee26cffc611995d2729efb478b1
FMalejki <FMalejki@proton.me>
2025-09-19 17:00:00 +0200

    feat: project milestone

--- lab3/ConditionNonStarv.java
+++ lab3/ConditionNonStarv.java
@@ -12,7 +12,7 @@ public class ConditionNonStarv {
     static final int N = 4;
     static final int BUFFER_SIZE = 10;
     static final long RUN_MS = 20_000;
-    
+
 
     static volatile boolean running = true;
 
@@ -22,6 +22,7 @@ public class ConditionNonStarv {
 
         List<Thread> threads = new ArrayList<>();
 
+        
         for (int i = 1; i <= M; i++) {
             String name = "Producer-" + i;
             stats.put(name, new Stats(name, Role.PRODUCER));
